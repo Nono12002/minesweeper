3563248604f173ec9dc4ed61fef78e603f374889
Nono12002 <127423527+Nono12002@users.noreply.github.com>
2023-03-13 10:08:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (true) {
+    	
+    }
+})
+let Flag_1 = game.createSprite(randint(0, 4), randint(0, 4))
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
